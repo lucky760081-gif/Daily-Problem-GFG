02db8cf094dc6e75a3e98bc433526bc66931a403
lucky760081-gif <lucky760081@gmail.com>
2026-07-02 17:36:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check Subset sum divisible by k/check-subset-sum-divisible-by-k.py
@@ -0,0 +1,19 @@
+class Solution:
+    def divisibleByK(self, arr, k):
+        # code here
+        dp = [False] * k
+
+        for num in arr:
+            ndp = dp[:]
+            ndp[num % k] = True
+
+            for r in range(k):
+                if dp[r]:
+                    ndp[(r + num) % k] = True
+
+            dp = ndp
+
+            if dp[0]:
+                return True
+
+        return False
