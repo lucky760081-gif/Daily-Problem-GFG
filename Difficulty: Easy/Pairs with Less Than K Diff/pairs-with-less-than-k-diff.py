345fa4759496661a7a2ac3945826311da3ce8ad0
lucky760081-gif <lucky760081@gmail.com>
2026-08-04 20:59:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairs with Less Than K Diff/pairs-with-less-than-k-diff.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countPairs(self, arr: list[int], k: int) -> int:
+        # code here
+        arr.sort()
+        n = len(arr)
+
+        ans = 0
+        j = 0
+
+        for i in range(n):
+            while j < n and arr[j] - arr[i] < k:
+                j += 1
+
+            ans += j - i - 1
+
+        return ans
