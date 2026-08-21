c3e1947511fb116fe958e423705d407fbdc6c7f1
lucky760081-gif <lucky760081@gmail.com>
2026-08-21 19:25:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Transform String/transform-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def transform(self, s1, s2): 
+        #code here
+        if len(s1) != len(s2):
+            return -1
+
+        from collections import Counter
+
+        if Counter(s1) != Counter(s2):
+            return -1
+
+        i = len(s1) - 1
+        j = len(s2) - 1
+
+        while i >= 0 and j >= 0:
+            if s1[i] == s2[j]:
+                j -= 1
+            i -= 1
+
+        return j + 1
